4ee5238ccfdad9bb09fa2ca31785efdf9aaae511
Daxin319 <lylehigg@gmail.com>
2024-10-29 20:16:34 -0600

    added function to split text nodes at markup delimiters

--- public/src/test_textnode.py
+++ public/src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import *
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -37,6 +37,55 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev/")
         node2 = TextNode("This is a text node", TextType.BOLD, "http://www.boot.dev/")
         self.assertNotEqual(node, node2)    
+
+
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_basic_split(self):
+        # Text with a pair of delimiters
+        nodes = [TextNode("Hello *world*!", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "*", TextType.BOLD)
+        
+        expected = [
+            TextNode("Hello "),
+            TextNode("world", TextType.BOLD),
+            TextNode("!")
+        ]
+        self.assertEqual(result, expected)
+    
+    def test_no_delimiter_found(self):
+        # Text without the delimiter should return original node
+        nodes = [TextNode("Hello world!", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "*", TextType.BOLD)
+        
+        expected = [TextNode("Hello world!", TextType.TEXT)]
+        self.assertEqual(result, expected)
+
+    def test_invalid_syntax_single_delimiter(self):
+        # Single delimiter, should raise an exception
+        nodes = [TextNode("Hello *world!", TextType.TEXT)]
+        with self.assertRaises(Exception):
+            split_nodes_delimiter(nodes, "*", TextType.BOLD)
+
+    def test_nested_delimiter_split(self):
+        # Nested delimiters, expecting recursive behavior
+        nodes = [TextNode("Hello *world* and *everyone*", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "*", TextType.BOLD)
+        
+        expected = [
+            TextNode("Hello "),
+            TextNode("world", TextType.BOLD),
+            TextNode(" and "),
+            TextNode("everyone", TextType.BOLD)
+        ]
+        self.assertEqual(result, expected)
+    
+    def test_non_text_type_node(self):
+        # Node is not of TextType.TEXT, should be returned unmodified
+        non_text_node = HTMLNode("div", "Some content")
+        result = split_nodes_delimiter([non_text_node], "*", TextType.BOLD)
+        
+        self.assertEqual(result, [non_text_node])
 
 if __name__ == "__main__":
     unittest.main()
--- public/src/textnode.py
+++ public/src/textnode.py
@@ -1,6 +1,7 @@
 from enum import Enum
-from leafnode import *
+from leafnode import * # leafnodes are also a child class of htmlnode
 
+# Enum to hold valid text types
 class TextType(Enum):
     HTML = "html"
     BOLD = "bold"
@@ -10,6 +11,7 @@ class TextType(Enum):
     LINK = "link"
     IMAGE = "image"
 
+#TextNode class, child of HTMLNode
 class TextNode:
     def __init__(self, text, text_type=TextType.TEXT, url=None):
         self.text = text
@@ -23,7 +25,7 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
-
+#function to convert textnodes to html formatted leafnodes
 def textnode_to_htmlnode(textnode):
     match textnode.text_type:
         case TextType.TEXT:
@@ -40,3 +42,29 @@ def textnode_to_htmlnode(textnode):
             return LeafNode('img', "", {'src': textnode.url, 'alt': textnode.text})
         case _:
             raise ValueError(f"Unexpected text type: {textnode.text_type}")
+
+# function to convert raw markdown to list of TextNodes        
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if not isinstance(node, TextNode) or node.text_type != TextType.TEXT :
+            new_nodes.append(node)
+        else:
+            first_pos = node.text.find(delimiter) #find first delimiter
+            second_pos = node.text.find(delimiter, first_pos + 1)
+            if second_pos == -1 and first_pos != -1:
+                raise Exception('<----------------------Invalid markdown syntax, no closing delimiter found or empty set of delimiters found---------------------->')
+            if first_pos == -1:
+                new_nodes.append(TextNode(node.text))
+            else:
+                if node.text[:first_pos]:
+                    new_nodes.append(TextNode(node.text[:first_pos]))
+                new_nodes.append(TextNode(node.text[first_pos + len(delimiter):second_pos], text_type))
+                if node.text[second_pos + len(delimiter):]:
+                    new_nodes.extend(split_nodes_delimiter([TextNode(node.text[second_pos + len(delimiter):])], delimiter, text_type))
+    return new_nodes
+                
+            
+
+debug_node = TextNode("This line has a `code block` section.")
+split_nodes_delimiter([debug_node], '`', TextType.CODE)
