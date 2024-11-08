5ef5cb5a0a888d2de1d948588c4af8a138f8a4e2
Daxin319 <lylehigg@gmail.com>
2024-11-07 20:49:08 -0700

    removed unnecessary imports from main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 from funcs import *
-from textnode import *
-from parentnode import *
-from leafnode import *
+
 
 def main():
     file_transfer("/home/lyle/workspace/github.com/Daxin319/Static-Site-Generator/static", "/home/lyle/workspace/github.com/Daxin319/Static-Site-Generator/public")
