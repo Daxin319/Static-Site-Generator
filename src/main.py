1f9ba12dfa5b51fbd1aaa56f06fd5ea90896ed22
Daxin319 <lylehigg@gmail.com>
2025-02-28 19:00:00 -0700

    updated to work with the new updated course on boot.dev

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from funcs import *
 
 
 def main():
-    file_transfer("/home/lyle/workspace/github.com/Daxin319/Static-Site-Generator/static", "/home/lyle/workspace/github.com/Daxin319/Static-Site-Generator/public")
+    file_transfer("/home/lyle/workspace/github.com/Daxin319/Static-Site-Generator/src/static", "/home/lyle/workspace/github.com/Daxin319/Static-Site-Generator/public")
     generate_pages_recursive("/home/lyle/workspace/github.com/Daxin319/Static-Site-Generator/src/content", "/home/lyle/workspace/github.com/Daxin319/Static-Site-Generator/template.html", "/home/lyle/workspace/github.com/Daxin319/Static-Site-Generator/public")
     print("<----------Generation Complete!---------->")
 
