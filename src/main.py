10cc3e220d307c145dddc091d640dd259e7799b6
Daxin319 <lylehigg@gmail.com>
2025-02-28 19:30:38 -0700

    fixed small bug (I think)

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ def main():
         basepath = "/"
     else:
         basepath = sys.argv[2]
+    print(basepath)
     file_transfer("/home/lyle/workspace/github.com/Daxin319/Static-Site-Generator/src/static", "/home/lyle/workspace/github.com/Daxin319/Static-Site-Generator/docs")
     generate_pages_recursive("/home/lyle/workspace/github.com/Daxin319/Static-Site-Generator/src/content", "/home/lyle/workspace/github.com/Daxin319/Static-Site-Generator/template.html", "/home/lyle/workspace/github.com/Daxin319/Static-Site-Generator/docs", basepath)
     print("<----------Generation Complete!---------->")
